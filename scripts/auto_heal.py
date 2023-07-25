c62a8f3b6930759ad12e14319d0a4489ec6188b7
Mauro Campos Pahoor <mauro.pahoor@gmail.com>
2023-07-25 14:49:59 -0300

    Add files via upload

---
+++ scripts/auto_heal.py
@@ -0,0 +1,19 @@
+import pyautogui as pg
+import keyboard
+import pytesseract as pt
+from PIL import Image
+import time
+
+def auto_heal():
+    pt.pytesseract.tesseract_cmd = r'..\tesseract\tesseract.exe'
+
+    while keyboard.is_pressed('g') == False:
+        healthBarLocation = pg.locateOnScreen('imgs\healthbar.png')
+        healthBar = pg.screenshot(region=(healthBarLocation.left, healthBarLocation.top, healthBarLocation.width + 60, healthBarLocation.height))
+        healthStatus = pt.image_to_string(healthBar)
+        print(f"Sua vida é {healthStatus}")
+        time.sleep(0.5)
+
+
+keyboard.wait('h')
+auto_heal()
